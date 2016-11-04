f3432593573e92e4ee66ecd5cbb44a740874508c
zhangjingxuan <m18222962017@163.com>
2016-11-04 15:56:26 +0800

    1.学生端点击查看作业详情

--- eteacher/src/com/turing/eteacher/service/IWorkService.java
+++ eteacher/src/com/turing/eteacher/service/IWorkService.java
@@ -11,6 +11,8 @@ public interface IWorkService extends IService<Work> {
 	public List<Map> getListForTable(String termId, String courseId);
 	//学生端获取作业列表
 	public List<Map> getListByStuId(String stuId, String status,int page);
+	//学生端查看作业详情
+	public Map getSWorkDetail(String workId);
 	
 	/**
 	 * 教师相关接口
